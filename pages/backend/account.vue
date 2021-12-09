379e2c693379be550bc01ed37714f43eb873dbda
Damien Goehrig <damien.goehrig@gmail.com>
2021-12-09 18:58:28 -0500

    Add token revocation

--- pages/backend/account.vue
+++ pages/backend/account.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="section">
     <div class="container">
-      <h2 class="title">My account</h2>
+      <h2 class="title">
+        My account
+      </h2>
       <div class="content">
         <p>
           <strong>Username:</strong>
@@ -12,6 +14,16 @@
           {{ loggedInUser.email }}
         </p>
       </div>
+      <h2 class="title">
+        Sessions
+      </h2>
+      <div v-for="token in tokenList" :key="token._id" class="item">
+        Token: {{ token.refreshToken.substring(0, 10) }}[...]{{ token.refreshToken.substring(token.refreshToken.length - 15) }}
+        Last used: {{ token.lastUsed }}
+        <button class="button is-danger" @click="revokeToken(token)">
+          Revoke
+        </button>
+      </div>
     </div>
   </section>
 </template>
@@ -20,10 +32,26 @@
 import { mapGetters } from 'vuex'
 
 export default {
+  layout: 'backend',
+  middleware: 'auth',
+  async asyncData ({ $axios }) {
+    const res = await $axios({ url: '/api/auth/userToken/' })
+    return { tokenList: res.data }
+  },
+  data () {
+    return {
+      tokenList: []
+    }
+  },
   computed: {
     ...mapGetters(['loggedInUser'])
   },
-  layout: 'backend',
-  middleware: 'auth',
+  methods: {
+    async revokeToken (token) {
+      await this.$axios.delete('/api/auth/userToken/' + token._id)
+      this.tokenList = this.tokenList.filter(t => t._id !== token._id)
+    }
+  }
+
 }
 </script>
